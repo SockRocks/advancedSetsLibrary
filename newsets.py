2b250abd3e5a1bb53526b6b6614257cebbf4badc
SockRocks <67668693+SockRocks@users.noreply.github.com>
2024-03-15 11:19:49 -0400

    Add files via upload
    Not a complete update (still bugs needing to be fixed). This commit simply updates the class constructor, which allows the user the to pass in any number of arguments, including elements that are lists themselves which will be converted into the Set object.

--- newsets.py
+++ newsets.py
@@ -1,16 +1,17 @@
-from findFactor import allSubsets
-import newLib
-
 class Set:
-    def __init__(self, elements, universe=[], multiSet=False):
-        self.elements = elements
+    def __init__(self, *elements, universe=[], multiSet=False):
         self.current = 0
-        
-        if not multiSet:
-            self.elements = self.duplicateRemoval()
+        self.multiSet = multiSet
+        if len(elements) == 1 and type(elements[0]) == list:
+            elements = elements[0]
 
+        if not self.multiSet:
+            elements = self.duplicateRemoval(elements)
+        self.universe = universe
+        self.elements = self.toSet(elements)
+    
     def __len__(self):
-        return len(self.duplicateRemoval())
+        return len(self.duplicateRemoval(self.elements))
 
     def __iter__(self):
         return self
@@ -29,8 +30,9 @@ class Set:
         else:
             self.current = 0
             raise StopIteration
+
     def __str__(self):
-        return str(list(self))
+        return str(self.__list__())
     
     def __list__(self):
         return_list = []
@@ -53,21 +55,108 @@ class Set:
             else:
                 return_list[i] = self.elements[i]
         return return_list
+
     def __set__(self):
         return set(self.__list__())
 
-    def duplicateRemoval(self):
+    def toSet(self, elements):
+        return_list = []
+        for i in range(len(elements)):
+            return_list.append(0)
+        range(len(elements))
+        for i in range(len(elements)):
+            mutable = False
+            
+            try:
+                testDict = dict()
+                testDict[elements[i]] = 1
+            except TypeError:
+                mutable =True
+                
+            if isinstance(elements[i], Set):
+                mutable = False
+            
+            if mutable:
+                return_list[i] = Set(elements[i], universe=self.universe, multiSet=self.multiSet)
+            else:
+                return_list[i] = elements[i]
+        return return_list
+        
+    def duplicateRemoval(self, elements):
         clean = []
-        for element in self.elements:
+        for element in elements:
             if element not in clean:
                 clean.append(element)
         return clean
 
-    def powerSet(self):
+    '''def powerSet(self):
         pSet = []
         for x in allSubsets(self.elements).values():
             for y in x:
                 pSet.append(Set(y))
         pSet.append(Set([]))
-        return Set(pSet)
+        return Set(pSet)'''
+        
+    @staticmethod
+    def genPSet(_set, size):
+        subsets = []
 
+        if size == len(_set):
+            subsets.append(_set)
+        elif size == 1:
+            for x in _set:
+                subsets.append([x])
+        else:
+            for x in _set:
+                for y in genPSet(_set, size-1):
+                    y.append(x)
+                    _y = clean(y)
+                    already = False
+                    for z in subsets:
+                        if equal(_y, z):
+                            already = True
+                            break
+                    if len(_y) == size and not already:
+                        subsets.append(_y)
+
+        return clean(subsets)
+    
+    def subsetof(seta, setb):
+        for y in seta:
+            if y not in setb:
+                return False
+        return True
+    def equal(seta, setb):
+        if subsetof(seta, setb) and subsetof(setb, seta):
+            return True
+        else:
+            return False
+
+    def clean(sets):
+        clean = []
+        for x in sets:
+            if x not in clean:
+                clean.append(x)
+        return clean
+
+    def calcPSetOf(seta):
+        powerSet = []
+        for i in range(1, len(seta)+1):
+            for x in genPSet(seta, i):
+                powerSet.append(x)
+        powerSet.append(list())
+        return powerSet
+
+
+    def powerSet(set):
+        pSet = []
+        for x in allSubsets(set).values():
+                for y in x:
+                    pSet.append(y)
+        pSet.append(list())
+        return pSet
+
+
+
+a = Set([1,2,3],2,3, 3, multiSet=True)
+print(a)
