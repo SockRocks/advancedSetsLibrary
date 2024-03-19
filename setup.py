b681907dec5f184773f8ecc946dea924deb39c00
SockRocks <67668693+SockRocks@users.noreply.github.com>
2024-03-19 00:04:25 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-import setuptools
+from setuptools import find_packages, setup, Extension
 
+with open('README.md', 'r')as f:
+    long_desc = f.read()
 
-with open('README.md', 'r') as fh:
-    description = fh.read()
-
-setuptools.setup(name='SetCalcPy',
-                 version='0.0.2',
-                 author='SockRocks',
-                 packages=['SetCalcPy'],
-                 long_description=description,
-                 long_description_content_type="text/markdown",
-                 url='https://github.com/SockRocks/Python-Elementary-Set-Theory-Calculator',
-                 license='MIT',
-                 python_requires='>=3.8',
-                 install_requires=[]
-                 )
+setup(
+    name='SetCalcPy',
+    ext_modules=[Extension(name='powerSetCalc', sources=['SetCalcPy/powerSet/powerSetCalc.c'])],
+    version='1.1.2',
+    description='A library for performing basic set theory operations.',
+    package_dir = {"Set":"SetCalcPy"},
+    packages = ['SetCalcPy'],
+    keywords=['calculator', 'homework-help', 'set-theory'],
+    long_description = long_desc,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/SockRocks/Python-Elementary-Set-Theory-Calculator',
+    author = 'Ethan Gaver',
+    author_email = 'ethaneggcode@gmail.com',
+    license = 'GNU',
+    extras_require = {
+        "dev":["twine >= 4.0.2"]
+        },
+    python_requires=">=3.11.5"
+)
