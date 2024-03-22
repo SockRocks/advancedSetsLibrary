8a049749b34d01fbf3f86f546f9da844d7764108
SockRocks <67668693+SockRocks@users.noreply.github.com>
2024-03-21 23:04:09 -0400

    Add files via upload

--- src/SetCalcPy/proofGenerator/proofCalculator.py
+++ src/SetCalcPy/proofGenerator/proofCalculator.py
@@ -32,10 +32,81 @@ class proofGen:
         return data
 
     def convertToLogical(self, setExpression):
-        
+        substitutions = {}
+        complements = {}
+        logical_statements = []
+
+        paren_count = setExpression.count('(')
+        for x in range(paren_count):
+            cur_exp = ''
+            ind = setExpression.find('(') + 1
+            while setExpression[ind] != ')':
+                cur_exp += setExpression[ind]
+                ind += 1
+            substitutions[f'#[{ind}]'] = cur_exp
+            setExpression = setExpression[:ind-len(cur_exp) - 1] + f'#[{ind}]' + setExpression[ind + 1:]
+
+        for x in range(setExpression.count('^c')):
+            ind = setExpression.find('c')
+            remaining = ''
+            n_ind = ind - 2
+            while setExpression[n_ind] != '+' and setExpression[n_ind] != '&' and setExpression[n_ind] != '*':
+                if setExpression[n_ind] != '+' and setExpression[n_ind] != '&' and setExpression[n_ind] != '*':
+                    remaining += setExpression[n_ind]
+                n_ind -= 1
+            remaining = remaining[::-1]
+            complements[f'@[{ind}]'] = remaining
+            setExpression = setExpression[:ind - len(remaining) - 1] + f'@[{ind}]' + setExpression[ind+1:]
+
+        print(setExpression)
+        if setExpression.startswith('x\in'):
+            setExpression = setExpression[4:]
+            print(setExpression)
+            ind = 0
+            for i in range(len(setExpression)):
+                if i >= ind:
+                    if i == 0:
+                        cur_exp = ''
+                        ind = 0
+                        opp_count = 0
+                        while opp_count < 2:
+                            add = 1 if not self.checkEqualOperator(setExpression[ind]) or ind == len(setExpression) - 1 else 0
+                            opp_count += add
+                            if self.checkEqualOperator(setExpression[ind]) or opp_count <= 1:
+                                cur_exp += setExpression[ind]
+                            ind += 1
+                        logical_statements.append(cur_exp)
+
+                    else:
+                        cur_exp = setExpression[i - 1]
+                        ind = i
+                        opp_count = 0
+                        while opp_count < 1:
+                            add = 1 if not self.checkEqualOperator(setExpression[ind]) or ind == len(setExpression) - 1 else 0
+                            opp_count += add
+                            if self.checkEqualOperator(setExpression[ind]):
+                                cur_exp += setExpression[ind]
+                            ind += 1
+                        logical_statements.append(cur_exp)
+
+            for i in range(len(logical_statements)):
+                new = ''
+                for y in logical_statements[i]:
+                    if y != ' ':
+                        new += y
+                    logical_statements[i] = new
+
+        print(logical_statements)
+
+
+        return False
+
+    def checkEqualOperator(self, statement):
+        return statement != '+' and statement != '&' and statement != '*'
 
     def proveSubsetOf(self):
-
+        LHS_logical = self.convertToLogical('x\in ' + self.LHS)
+        #RHS_logical  = self.convertToLogical('x\in' + self.RHS)
 
 
 
@@ -47,3 +118,7 @@ def generateProof(statement):
     :return: A list to be printed as a proof of the statement
     """
     proofGen(statement)
+
+
+generateProof('C+(A+B)^c<(A^c&B^c)+C')
+#generateProof('C+B+D+K+U<D+B')
